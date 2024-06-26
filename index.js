80992c7d463eb03d47bac46c080039cccc756794
cricket lover <sami76748@gmail.com>
2024-06-26 06:51:55 -0700

    vercel

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 import express from "express"
+import cors from "cors"
 const app = express();
 const port = 3000;
 
 // Root route
+// app.use(cookieParser());
+app.use(express.json());
+app.use(cors({  
+    origin:'', // Replace with your frontend origin
+    credentials: true,
+}));
+
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
@@ -12,6 +20,11 @@ app.get('/about', (req, res) => {
   res.send('This is the about page.');
 });
 
+app.use((req, res, next) => {
+    res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+    res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+    next();
+  });
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
